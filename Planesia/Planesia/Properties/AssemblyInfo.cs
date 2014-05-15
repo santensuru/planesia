a5a95909fffa529934d1f39e8109ad89e54c77c3
Yusro Tsaqova <yusrotsaqova@gmail.com>
2014-05-15 18:06:00 +0700

    Planesia

--- Planesia/Planesia/Properties/AssemblyInfo.cs
+++ Planesia/Planesia/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5e831e5-547b-475d-a96c-d5f165d91e12")]
+[assembly: Guid("3d2ac555-60f2-4fa8-a080-910fca5fc282")]
 
 // Version information for an assembly consists of the following four values:
 //
