2a2cc23e2a77ce0482160f5cca2e96657f140c41
Radhea Permata Dewi <radhea.dewi@gmail.com>
2014-05-17 12:50:54 +0700

    Testing insert gambar

--- Planesia/Planesia/Controllers/HomeController.cs
+++ Planesia/Planesia/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace Planesia.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Map()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
