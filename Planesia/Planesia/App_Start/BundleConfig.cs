3911fdb7659db06c32373cfd308a23aaa7ad8452
Radhea Permata Dewi <radhea.dewi@gmail.com>
2014-06-11 20:30:05 +0700

    Bootstrap Modal

--- Planesia/Planesia/App_Start/BundleConfig.cs
+++ Planesia/Planesia/App_Start/BundleConfig.cs
@@ -25,7 +25,6 @@ namespace Planesia
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      
                       "~/Content/Style.css",
                       "~/Content/PagedList.css"));
         }
