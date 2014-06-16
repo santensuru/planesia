31eed780a71149fc50ed75c29ef3659987f3fcfd
santensuru <djuned.ong@gmail.com>
2014-06-16 23:20:07 +0700

    Fauna and Flora testclass

---
+++ Planesia/Planesia/UnitTest/FaunaRepositoryTest.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Planesia.Repository;
+using Planesia.Models;
+
+namespace Planesia.UnitTest
+{
+    [TestClass]
+    public class FaunaRepositoryTest
+    {
+        [TestMethod]
+        public void FaunaRepositoryDb_GetAllData()
+        {
+            FaunaRepositoryDb urd = new FaunaRepositoryDb();
+            int expectedCount = 3;
+
+            int count = urd.Faunas.Count;
+
+            Assert.AreSame(expectedCount, count, "Harusnya 3.");
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryMock_GetAllData()
+        {
+            FaunaRepositoryMock urm = new FaunaRepositoryMock();
+            int expectedCount = 5;
+
+            int count = urm.Faunas.Count;
+
+            Assert.AreEqual(expectedCount, count, "Harusnya 5.");
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryDb_GetFaunaById()
+        {
+            FaunaRepositoryDb urd = new FaunaRepositoryDb();
+            var a = urd.GetFaunaById(1);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryMock_GetFaunaById()
+        {
+            FaunaRepositoryMock urm = new FaunaRepositoryMock();
+            var a = urm.GetFaunaById(1);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryDb_AddFauna()
+        {
+            FaunaRepositoryDb urd = new FaunaRepositoryDb();
+            Fauna fauna = new Fauna() { FaunaName = "Njajal Gan", FaunaId = 1000 };
+            urd.AddFauna(fauna);
+            var a = urd.GetFaunaById(1000);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryMock_AddFauna()
+        {
+            FaunaRepositoryMock urm = new FaunaRepositoryMock();
+            Fauna fauna = new Fauna() { FaunaName = "Njajal Gan", FaunaId = 1000 };
+            urm.AddFauna(fauna);
+            var a = urm.GetFaunaById(1000);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryDb_UpdateFauna()
+        {
+            FaunaRepositoryDb urd = new FaunaRepositoryDb();
+            Fauna fauna = new Fauna() { FaunaName = "Njajal Gan", FaunaId = 1 };
+            urd.UpdateFauna(fauna);
+            var a = urd.GetFaunaById(1);
+
+            Assert.AreEqual(fauna, a);
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryMock_UpdateFauna()
+        {
+            FaunaRepositoryMock urm = new FaunaRepositoryMock();
+            Fauna fauna = new Fauna() { FaunaName = "Njajal Gan", FaunaId = 1 };
+            urm.UpdateFauna(fauna);
+            var a = urm.GetFaunaById(1);
+
+            Assert.AreEqual(fauna, a);
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryDb_DeleteFauna()
+        {
+            FaunaRepositoryDb urd = new FaunaRepositoryDb();
+            urd.DeleteFauna(1);
+            var a = urd.GetFaunaById(1);
+
+            Assert.IsNull(a);
+        }
+
+        [TestMethod]
+        public void FaunaRepositoryMock_DeleteFauna()
+        {
+            FaunaRepositoryMock urm = new FaunaRepositoryMock();
+            urm.DeleteFauna(1);
+            var a = urm.GetFaunaById(1);
+
+            Assert.IsNull(a);
+        }
+    }
+}
---
+++ Planesia/Planesia/UnitTest/FloraRepositoryTest.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Planesia.Repository;
+using Planesia.Models;
+
+namespace Planesia.UnitTest
+{
+    [TestClass]
+    public class FloraRepositoryTest
+    {
+        [TestMethod]
+        public void FloraRepositoryDb_GetAllData()
+        {
+            FloraRepositoryDb urd = new FloraRepositoryDb();
+            int expectedCount = 3;
+
+            int count = urd.Floras.Count;
+
+            Assert.AreSame(expectedCount, count, "Harusnya 3.");
+        }
+
+        [TestMethod]
+        public void FloraRepositoryMock_GetAllData()
+        {
+            FloraRepositoryMock urm = new FloraRepositoryMock();
+            int expectedCount = 5;
+
+            int count = urm.Floras.Count;
+
+            Assert.AreEqual(expectedCount, count, "Harusnya 5.");
+        }
+
+        [TestMethod]
+        public void FloraRepositoryDb_GetFloraById()
+        {
+            FloraRepositoryDb urd = new FloraRepositoryDb();
+            var a = urd.GetFloraById(1);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FloraRepositoryMock_GetFloraById()
+        {
+            FloraRepositoryMock urm = new FloraRepositoryMock();
+            var a = urm.GetFloraById(1);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FloraRepositoryDb_AddFlora()
+        {
+            FloraRepositoryDb urd = new FloraRepositoryDb();
+            Flora flora = new Flora() { FloraName = "Njajal Gan", FloraId = 1000 };
+            urd.AddFlora(flora);
+            var a = urd.GetFloraById(1000);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FloraRepositoryMock_AddFlora()
+        {
+            FloraRepositoryMock urm = new FloraRepositoryMock();
+            Flora flora = new Flora() { FloraName = "Njajal Gan", FloraId = 1000 };
+            urm.AddFlora(flora);
+            var a = urm.GetFloraById(1000);
+
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void FloraRepositoryDb_UpdateFlora()
+        {
+            FloraRepositoryDb urd = new FloraRepositoryDb();
+            Flora flora = new Flora() { FloraName = "Njajal Gan", FloraId = 1 };
+            urd.UpdateFlora(flora);
+            var a = urd.GetFloraById(1);
+
+            Assert.AreEqual(flora, a);
+        }
+
+        [TestMethod]
+        public void FloraRepositoryMock_UpdateFlora()
+        {
+            FloraRepositoryMock urm = new FloraRepositoryMock();
+            Flora flora = new Flora() { FloraName = "Njajal Gan", FloraId = 1 };
+            urm.UpdateFlora(flora);
+            var a = urm.GetFloraById(1);
+
+            Assert.AreEqual(flora, a);
+        }
+
+        [TestMethod]
+        public void FloraRepositoryDb_DeleteFlora()
+        {
+            FloraRepositoryDb urd = new FloraRepositoryDb();
+            urd.DeleteFlora(1);
+            var a = urd.GetFloraById(1);
+
+            Assert.IsNull(a);
+        }
+
+        [TestMethod]
+        public void FloraRepositoryMock_DeleteFlora()
+        {
+            FloraRepositoryMock urm = new FloraRepositoryMock();
+            urm.DeleteFlora(1);
+            var a = urm.GetFloraById(1);
+
+            Assert.IsNull(a);
+        }
+    }
+}
